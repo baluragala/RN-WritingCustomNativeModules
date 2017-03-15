b7533a7e9862855bc960b2f15d0e63c7d03c5a65
Bala Krishna Ragala <baluragala@sparity.com>
2017-03-15 16:13:02 +0530

    step-8 android-native callbacks added- android completed

--- android/app/src/main/java/com/writingcustomnativemodules/LibraryManager.java
+++ android/app/src/main/java/com/writingcustomnativemodules/LibraryManager.java
@@ -4,15 +4,20 @@ import android.app.Activity;
 import android.content.Intent;
 
 import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+
+import static android.R.attr.data;
 
 /**
  * Created by moksha on 15/03/17.
  */
 
 public class LibraryManager extends ReactContextBaseJavaModule implements ActivityEventListener {
+
+    private Callback mCallback;
 
     public LibraryManager(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -27,7 +32,8 @@ public class LibraryManager extends ReactContextBaseJavaModule implements Activi
 
 
     @ReactMethod
-    public void selectImage() {
+    public void selectImage(Callback callback) {
+        mCallback = callback;
         Activity currentActivity = getCurrentActivity();
         Intent libraryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         currentActivity.startActivityForResult(libraryIntent, 1);
@@ -36,6 +42,8 @@ public class LibraryManager extends ReactContextBaseJavaModule implements Activi
     @Override
     public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
         String filePath = data != null ? data.getDataString() : "";
+        if (filePath.length() > 0)
+            mCallback.invoke(filePath);
     }
 
     @Override
--- index.android.js
+++ index.android.js
@@ -11,25 +11,54 @@ import {
   Text,
   Button,
   View,
-  TouchableOpacity,
+  Image,
   NativeModules
 } from 'react-native';
 import * as globalStyles from './style.global';
 
 const {LibraryManager} = NativeModules;
 
-console.log(LibraryManager);
+console.log(NativeModules);
 
 export default class WritingCustomNativeModules extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.onSelectImage = this.onSelectImage.bind(this);
+    this.renderProfileImage = this.renderProfileImage.bind(this);
+  }
+
+  onSelectImage() {
+    LibraryManager.selectImage((filePath) => {
+      console.log("onSelectImage", filePath);
+      this.setState({selectedImageUrl: filePath})
+    });
+  }
+
+  renderProfileImage() {
+    if (this.state.selectedImageUrl) {
+      return (
+        <Image
+          source={{ uri: this.state.selectedImageUrl }}
+          style={styles.selectedImage}
+        />
+      );
+    }
+    return (
+      <Button
+        onPress={this.onSelectImage}
+        title="Select Image"
+        color="#841584"
+        accessibilityLabel="Click to load image"
+      />
+    );
+  }
+
   render() {
     return (
       <View style={[globalStyles.COMMON_STYLES.pageContainer, styles.container]}>
-        <Button
-          onPress={() => LibraryManager.selectImage()}
-          title="Select Image"
-          color="#841584"
-          accessibilityLabel="Click to load image"
-        />
+        {this.renderProfileImage()}
       </View>
     );
   }
@@ -39,6 +68,10 @@ const styles = StyleSheet.create({
   container: {
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  selectedImage: {
+    width: 250,
+    height: 250
   }
 });
 
